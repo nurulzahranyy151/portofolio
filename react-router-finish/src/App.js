110ca9139b881207d5912a93d6238a9ef57c0041
USER <nurulqalbizahrani11@gmail.com>
2024-08-31 15:48:28 +0800

    tes

--- react-router-finish/src/App.js
+++ react-router-finish/src/App.js
@@ -9,97 +9,95 @@ import ProtectedRoute from './pages/ProtectedRoute';
 import PageNotFound from './pages/PageNotFound';
 import Overview from './pages/Overview';
 import Stats from './pages/Stats';
-import Users from './pages/Users';
+import UserProfile from './pages/UserProfile';
 import UserDetails from './pages/UserDetails';
+import UserSettings from './pages/UserSettings';
+import usersData from './data/UserData';
 
 function App() {
-  const [isLoggedIn, setisLoggedIn] = useState(false);
+const [isLoggedIn, setisLoggedIn] = useState(false);
+const handleAuth = () => {
+setisLoggedIn(!isLoggedIn);
+};
+return (
+<Router>
+<div className="App">
+<nav className="topnav">
+<div>
+<ul>
+<li>
+<Link to="/">Home</Link>
+</li>
+<li>
+<Link to="/about">About</Link>
+</li>
+<li>
+<Link to="/contact">Contact</Link>
+</li>
+{isLoggedIn && (
+<>
+<li>
+<Link to="/dashboard">Dashboard</Link>
+</li>
+<li>
+<Link to="/user">User Profile</Link>
+</li>
+</>
+)}
+</ul>
+<button onClick={handleAuth}>
+{isLoggedIn ? 'Logout' : 'Login'}
+</button>
+</div>
+</nav>
+<Routes>
+<Route path="/" element={<Home />} />
 
-  const handleAuth = () => {
-    setisLoggedIn(!isLoggedIn);
-  };
-
-  return (
-    <Router>
-      <div className="App">
-        <nav className="topnav">
-          <div>
-            <ul>
-              <Link to="/">
-                <li>Home</li>
-              </Link>
-              <Link to="/about">
-                <li>About</li>
-              </Link>
-              <Link to="/contact">
-                <li>Contact</li>
-              </Link>
-              {isLoggedIn && (
-                <>
-                  <Link to="/dashboard">
-                    <li>Dashboard</li>
-                  </Link>
-                  <Link to="/user">
-                    <li>User Profile</li>
-                  </Link>
-                </>
-              )}
-            </ul>
-            <button onClick={handleAuth}>
-              {isLoggedIn ? 'Logout' : 'Login'}
-            </button>
-          </div>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<PageNotFound />} />
-          <Route
-            path="/dashboard/*"
-            element={
-              <ProtectedRoute
-                element={<Dashboard />}
-                isAuthenticated={isLoggedIn}
-              />
-            }>
-            <Route path="overview" element={<Overview />} />
-            <Route path="stats" element={<Stats />} />
-          </Route>
-          <Route
-            path="/about"
-            element={
-              <ProtectedRoute
-                element={<About />}
-                isAuthenticated={isLoggedIn}
-              />
-            }
-          />
-          <Route
-            path="/user/*"
-            element={
-              <ProtectedRoute
-                element={<Users />}
-                isAuthenticated={isLoggedIn}
-              />
-            }>
-            {/* <Route path="details/:userId/*" element={<UserDetails />} /> */}
-          </Route>
-
-          <Route
-            path="/user/details/:userId/*"
-            element={
-              <ProtectedRoute
-                element={<UserDetails />}
-                isAuthenticated={isLoggedIn}
-              />
-            }></Route>
-          {/* Tambahkan rute login jika perlu */}
-        </Routes>
-      </div>
-    </Router>
-  );
+<Route path="/contact" element={<Contact />} />
+<Route path="*" element={<PageNotFound />} />
+<Route
+path="/dashboard/*"
+element={
+<ProtectedRoute
+element={<Dashboard />}
+isAuthenticated={isLoggedIn}
+/>
+}>
+<Route path="overview" element={<Overview />} />
+<Route path="stats" element={<Stats />} />
+</Route>
+<Route
+path="/about"
+element={
+<ProtectedRoute
+element={<About />}
+isAuthenticated={isLoggedIn}
+/>
 }
-
+/>
+<Route
+path="/user/*"
+element={
+<ProtectedRoute
+element={<UserProfile />}
+isAuthenticated={isLoggedIn}
+/>
+}>
+{/* <Route path="details/:userId/*" element={<UserDetails />} /> */}
+<Route path="settings/:userId/*" element={<UserSettings />} />
+</Route>
+<Route
+path="/user/details/:userId/*"
+element={
+<ProtectedRoute
+element={<UserDetails />}
+isAuthenticated={isLoggedIn}
+/>
+}></Route>
+{/* Tambahkan rute login jika perlu */}
+</Routes>
+</div>
+</Router>
+);
+}
 export default App;
